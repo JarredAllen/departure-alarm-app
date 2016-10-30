4353f0a3db0410a7eef9f61bd8a20570b443a4ce
JarredAllen <mulan15262@gmail.com>
2016-10-30 15:31:58 -0700

    I did more stuff

--- src/main/java/com/example/jarred/departurealarm/CreateNotificationsService.java
+++ src/main/java/com/example/jarred/departurealarm/CreateNotificationsService.java
@@ -5,10 +5,16 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+/**
+ * The service that handles notifying the user of things that are going on in the background.
+ *
+ * @author Jarred
+ * @version 10/30/2016
+ */
 public class CreateNotificationsService extends IntentService {
 
     public CreateNotificationsService() {
-        super("");
+        super("CreateNotificationsService");
     }
 
     @Override
